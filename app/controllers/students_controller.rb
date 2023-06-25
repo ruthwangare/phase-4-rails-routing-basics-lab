1cb2c31ec8923d6c5c2c02c98776c2f07c749dab
Ruth <rwangare124@gmail.com>
2023-06-25 21:18:07 +0300

    Completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,4 +7,11 @@ class StudentsController < ApplicationController
    students = Student.order(grade: :desc)
    render json: students
  end
+
+ def highest_grade
+  student = Student.order(grade: :desc).first
+  render json: student
+ end
+
 end
+
